c66231d013243b0405fa51173b39d90ce9831fe1
Ashish-Yadav123 <arao43331@gmail.com>
2024-01-19 12:51:53 +0530

    set width transition

--- pages/components/BoxWidth.jsx
+++ pages/components/BoxWidth.jsx
@@ -11,9 +11,9 @@ const BoxWidth = () => {
 
   const calculateWidth = (index) => {
     if (activeImg === null || activeImg === index) {
-      return 30;
+      return 40;
     } else {
-      return 20;
+      return 23;
     }
   };
 
@@ -23,25 +23,28 @@ const BoxWidth = () => {
         <div className="xl:max-w-[1140px] 2xl:max-w-[1320px] mx-auto px-3 md:px-5 xl:px-0">
           <div className="flex items-center justify-between gap-4">
             {[0, 1, 2, 3].map((index) => (
+              //   <div
+              //     key={index}
+              //     onClick={() => handleImgClick(index)}
+              //     className={`max-w-[${calculateWidth(
+              //       index
+              //     )}%] cursor-pointer rounded-lg`}
+              //   >
               <div
                 key={index}
                 onClick={() => handleImgClick(index)}
-                className={`max-w-[${calculateWidth(
-                  index
-                )}%] overflow-hidden cursor-pointer`}
+                style={{
+                  width: `${calculateWidth(index)}%`,
+                  transition: "width 0.3s ease-in-out",
+                }}
+                className="cursor-pointer rounded-lg"
               >
-                <div className="flex justify-between gap-5 bg-[blue] text-white h-[300px] w-full p-5">
+                <div className="bg-[blue] text-white h-[300px] w-full p-2 rounded-lg">
                   <Image
-                    className="max-w-[200px] w-full object-cover"
+                    className="h-full w-full object-cover"
                     src={girlImg}
                     alt={`girl-img`}
                   />
-                  <p className="">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Culpa quibusdam sapiente possimus, cumque delectus amet
-                    aspernatur. Lorem ipsum dolor sit amet consectetur
-                    adipisicing elit.
-                  </p>
                 </div>
               </div>
             ))}

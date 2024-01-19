7147b44dcfd9dab527a173ad2813b38ee8b21825
Ashish-Yadav123 <arao43331@gmail.com>
2024-01-19 15:35:23 +0530

    remove extra file and merge code in 1 file

--- pages/components/BoxWidth.jsx
+++ pages/components/BoxWidth.jsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import React, { useState, useEffect } from "react";
 import girlImg from "../../public/assets/webp/girl_img.webp";
-import MobileBoxWidth from "./MobileBoxWidth";
 
 const BoxWidth = () => {
   const [activeImg, setActiveImg] = useState(0);
   const [windowWidth, setWindowWidth] = useState(0);
+  const [windowHeight, setWindowHeight] = useState(0);
 
   // HANDLE RESIZE WIDTH
   useEffect(() => {
@@ -16,6 +16,18 @@ const BoxWidth = () => {
     window.addEventListener("resize", handleResize);
     return () => {
       window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  // HANDLE RESIZE HEIGHT
+  useEffect(() => {
+    const handleResizeHeight = () => {
+      setWindowHeight(window.innerHeight);
+    };
+    setWindowHeight(window.innerHeight);
+    window.addEventListener("resize", handleResizeHeight);
+    return () => {
+      window.removeEventListener("resize", handleResizeHeight);
     };
   }, []);
 
@@ -37,11 +49,62 @@ const BoxWidth = () => {
     }
   };
 
+  const calculateHeight = (index) => {
+    if (windowHeight < 1024) {
+      if (activeImg === null || activeImg === index) {
+        return "650px";
+      } else {
+        return "200px";
+      }
+    }
+  };
+
   return (
     <>
       <div className="flex items-center justify-center w-full min-h-screen py-20">
         <div className="xl:max-w-[1140px] 2xl:max-w-[1320px] mx-auto px-3 md:px-5 xl:px-0">
-          <MobileBoxWidth />
+          <div className="md:hidden">
+            {[0, 1, 2, 3].map((index) => (
+              <div
+                key={index}
+                onClick={() => handleImgClick(index)}
+                style={{
+                  transition: "height 0.3s ease-in-out",
+                  height: calculateHeight(index),
+                }}
+                className="cursor-pointer rounded-lg mb-4 overflow-hidden"
+              >
+                <div className="bg-[blue] text-white max-w-[500px] mx-auto p-2 rounded-lg h-full">
+                  <Image
+                    className="h-full max-h-[300px] w-full object-cover"
+                    src={girlImg}
+                    alt={`girl-img`}
+                  />
+                  <p className="mt-4">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Recusandae atque distinctio quae eos, quidem architecto
+                    laudantium mollitia. Reiciendis facere optio fugiat
+                    temporibus dignissimos labore ex suscipit alias veritatis
+                    necessitatibus! Nulla!
+                  </p>
+                  <p className="mt-4">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Recusandae atque distinctio quae eos, quidem architecto
+                    laudantium mollitia. Reiciendis facere optio fugiat
+                    temporibus dignissimos labore ex suscipit alias veritatis
+                    necessitatibus! Nulla!
+                  </p>
+                  <p className="mt-4">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Recusandae atque distinctio quae eos, quidem architecto
+                    laudantium mollitia. Reiciendis facere optio fugiat
+                    temporibus dignissimos labore ex suscipit alias veritatis
+                    necessitatibus! Nulla!
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
           <div className="hidden md:flex flex-wrap lg:flex-nowrap items-center justify-start lg:justify-between gap-4">
             {[0, 1, 2, 3].map((index) => (
               <div
